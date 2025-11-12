9fb61613fd25fa5fd6c244ebd4484e802c2d9e22
SAMANTHA PARKES <slmillward@outlook.com>
2025-11-12 13:57:04 +0000

    button

--- src/app/error.js
+++ src/app/error.js
@@ -1,10 +1,13 @@
 "use client";
 
+import { signIn } from "next-auth/react";
+
 export default function ErrorPage({ error }) {
   return (
     <div className="flex flex-col items-center mt-10">
-      <h1>An error has occurred 👎🏼</h1>
-      <p>{error.message}</p>
+      <h1>We&apos;re sorry. An error has occurred 👎🏼</h1>
+      <p>Please make sure you&apos;re signed in to continue</p>
+      <button onClick={() => signIn("github")}>Sign in</button>
     </div>
   );
 }
